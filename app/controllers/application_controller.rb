d1078b1471d4f945c79dff078ac5cb7eaf2d9414
leedonggyu <zkfmapf123@naver.com>
2020-09-17 20:03:01 +0900

    202020

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,14 +27,14 @@ class ApplicationController < ActionController::Base
         begin
             @index = args[:index].find(params[:id])
             if args[:index] == Lesson
-                if @index.lesson_eval.find_by_user_id(current_user().id).valid?
-                    raise "error"
-                end
+                #if @index.lesson_eval.find_by_user_id(current_user().id).valid?
+                #    raise "error"
+                #end
                 @indexNumber = ((@index.score.to_f) / (@index.lesson_evals.count.to_f)).round(1)
             elsif args[:index] == Professor
-                if @index.professor_evals.find_by_user_id(current_user().id).valie?
-                    raise "error"
-                end
+                #if @index.professor_evals.find_by_user_id(current_user().id).valid?
+                #    raise "error"
+                #end
                 @indexNumber = ((@index.score.to_f) / (@index.professor_evals.count.to_f)).round(1)
             end
             @index.view += 1
